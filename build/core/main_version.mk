b442804d9087b9dcb2b0cf7a49e13ae22b4034d9
Ar5646L <aryan.v.bhatt@gmail.com>
2023-03-08 10:06:03 +0000

    Welcome to ActualRealityOS

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,8 +1,11 @@
+# Actual Reality Version
+AROS_VERSION := renegade
+
 # Build fingerprint
 ifeq ($(BUILD_FINGERPRINT),)
 BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
 CUSTOM_DEVICE ?= $(TARGET_DEVICE)
-ifneq ($(filter OFFICIAL,$(CUSTOM_BUILD_TYPE)),)
+ifneq ($(filter OFFICIAL,$(AROS_BUILD_TYPE)),)
 BUILD_SIGNATURE_KEYS := release-keys
 else
 BUILD_SIGNATURE_KEYS := test-keys
@@ -23,9 +26,10 @@ CUSTOM_SECURITY_PATCH := 2022-08-05
 
 # Versioning props
 ADDITIONAL_SYSTEM_PROPERTIES  += \
-    org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
-    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE) \
-    org.pixelexperience.build_security_patch=$(CUSTOM_SECURITY_PATCH)
+    org.actualrealityos.version=$(AROS_VERSION) \
+    ro.aros.version.display=$(AROS_VERSION) \
+    org.actualrealityos.build_date=$(CUSTOM_BUILD_DATE) \
+    org.actualrealityos.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    org.actualrealityos.build_type=$(AROS_BUILD_TYPE) \
+    org.actualrealityos.codename=$(AROS_CODENAME) \
+    org.actualrealityos.maintainer=$(AROS_MAINTAINER)
